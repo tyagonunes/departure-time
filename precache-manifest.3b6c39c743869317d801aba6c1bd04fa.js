04b6567ccfd1e9dd3fb57908110ad2a54825ecce
Tyago Nunes <tyagonunes@gmail.com>
2020-06-29 10:40:06 -0300

    Updates

---
+++ precache-manifest.3b6c39c743869317d801aba6c1bd04fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e0c6863de72ab4a86dcc7880f98bb7",
+    "url": "/departure-time/index.html"
+  },
+  {
+    "revision": "fa66754825b7aae7cc93",
+    "url": "/departure-time/static/css/2.826c19ff.chunk.css"
+  },
+  {
+    "revision": "fa66754825b7aae7cc93",
+    "url": "/departure-time/static/js/2.68a81624.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/departure-time/static/js/2.68a81624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eace1961eaf8ebf157c",
+    "url": "/departure-time/static/js/main.6df5b5e7.chunk.js"
+  },
+  {
+    "revision": "efd969ea00793d4f88e5",
+    "url": "/departure-time/static/js/runtime-main.6e11c051.js"
+  }
+]);
